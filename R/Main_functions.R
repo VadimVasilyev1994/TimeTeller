6aa084abd93d6205f9b7e2294977a0e042309fed
Vadim <vadim.vasilyev@warwick.ac.uk>
2023-12-13 12:58:55 +0000

    Adding documentation for cross-validation

--- R/Main_functions.R
+++ R/Main_functions.R
@@ -3,7 +3,7 @@
 #' Trains the model using provided expression matrix and metadata.
 #'
 #' @param exp_matrix Matrix or data frame with features in rows and samples in columns
-#' @param genes genes used to construct the timeteller model
+#' @param genes genes used to train the TimeTeller model
 #' @param group_1 vector containing metadata for each sample (eg individual, organ)
 #' @param group_2 vector containing metadata for each sample (eg individual, organ). Leave empty if unavailable
 #' @param group_3 vector containing metadata for each sample (eg individual, organ). Leave empty if unavailable
@@ -124,6 +124,55 @@ train_model <- function(exp_matrix, genes, group_1, group_2, group_3, time, repl
   }
   return(object)
 }
+
+#' Cross-validate the TimeTeller model
+#'
+#' Cross-validation of the TimeTeller model using provided expression matrix and metadata. Single or multiple groups can be selected for CV
+#'
+#' @param group_to_leave_out group to leave out for cross-validation. Must be one of the \code{'group_1'}, \code{'group_2'}, \code{'group_3'} or \code{'replicate'} supplied below
+#' @param genes genes used to train the TimeTeller model
+#' @param exp_matrix matrix or data frame with features in rows and samples in columns
+#' @param test_grouping_vars group variables (eg \code{'group_1'}, \code{'group_2'} or their combination \code{c('group_1','group_2')}) used for \code{timecourse_matched} normalisation. For cross-validation this will be the same as \code{grouping_vars}
+#' @param group_1 vector containing metadata for each sample (eg individual, organ)
+#' @param group_2 vector containing metadata for each sample (eg individual, organ). Leave empty if unavailable
+#' @param group_3 vector containing metadata for each sample (eg individual, organ). Leave empty if unavailable
+#' @param time vector containing timing of each sample. Please appreciate the difference between CT and ZT
+#' @param replicate vector containing replicate information if available
+#' @param mat_normalised was the expression matrix supplied already normalised (eg RMA for microarray, CPM/TPM for RNA, etc) or not. If not, \code{edgeR::cpm} function will be used (pseudocount of 2) to obtain log normalised values for downstream analysis. Default is TRUE
+#' @param treat_independently should replicates be averaged (might be useful for technical replicates, not recommended for biological replicates). Default is TRUE
+#' @param combine_for_norm should replicates be combined for normalisation. If TRUE, replicates will be combined for normalisation purposes (eg in \code{timecourse} normalisation, replicates will be combined and treated as one timeseries). Default is FALSE
+#' @param parallel_comp if TRUE, parallel computation using \code{foreach} and \code{doParallel} packages will be used. Default is FALSE
+#' @param cores if using parallel computation, how many cores should be used. Default is 4
+#' @param method method used for normalisation. Must be one of the \code{'intergene'}, \code{'timecourse'}, \code{'timecourse_matched'}, \code{'clr'}
+#' @param grouping_vars group variables (eg \code{'group_1'}, \code{'group_2'} or their combination \code{c('group_1','group_2')}) used for \code{timecourse_matched} normalisation
+#' @param num_PC how many principal components should be used for the analysis. Default is 3
+#' @param log_thresh log threshold selected. This is an important parameter and should be chosen carefully. Please read \code{help(choose_logthresh_plot)} for further information
+#' @param epsilon hyperparameter value used in theta calculation. No reason to change this, more information at \url{https://www.biorxiv.org/content/10.1101/2023.03.14.532177v1}. Default is 0.40
+#' @param eta hyperparameter value used in theta calculation. No reason to change this, more information at \url{https://www.biorxiv.org/content/10.1101/2023.03.14.532177v1}. Default is 0.35
+#' @param cov_estimate type of covariance estimate used. If \code{'normal'} is selected, \code{Rfast::mvnorm.mle} will be used. If \code{'robust'} is selected, \code{rrcov::CovMcd} will be used instead. This can be useful when there is a lot of training data, some of which is suspected to have high technical noise. Since we don't want to ignore biological noise (eg chronotype) and it's often hard to tell the two apart, default is \code{'normal'}
+#' @param alpha_par this is following \code{rrcov} package (in case \code{'robust'} was chosen above) and is the parameter controlling the size of the subsets over which the determinant will be minimized. Allowed values are between 0.5 and 1
+#' @param num_interp_points number of interpolation points to use for the mean spline and covariance matrices. Default is 144 and this corresponds to 24 * 60 / 144 = 10 minutes
+#' @param interp_method method used for spline interpolation. Must be one of the \code{'perpchip'} (which uses an adapted periodic version of \code{pchip} from \code{pracma} package) or \code{'standard'} (which uses periodic version of standard \code{spline} function from \code{splines} package).
+#' @param cov_path method for covariance matrix interpolation. Must be one of the \code{'spline'} (in which case covariance matrix will be interpolated element-wise using \code{interp_method} selection) or \code{'fisherrao'} (in which case the geodesics based on the Fisher–Rao metric for Gaussian distributions will be used).
+#' @param minpeakheight this is used internally with \code{pracma} package to locate peaks. No reason to change, however included for future testing / development. Please consult \code{?pracma::findpeaks}, if needed, for further information
+#' @param minpeakdistance this is used internally with \code{pracma} package to locate peaks. No reason to change, however included for future testing / development
+#' @param nups this is used internally with \code{pracma} package to locate peaks. No reason to change, however included for future testing / development
+#' @param ndowns this is used internally with \code{pracma} package to locate peaks. No reason to change, however included for future testing / development
+#' @param threshold this is used internally with \code{pracma} package to locate peaks. No reason to change, however included for future testing / development
+#' @param npeaks this is used internally with \code{pracma} package to locate peaks. No reason to change, however included for future testing / development
+#'
+#'
+#' @author Vadim Vasilyev
+#'
+#'
+#'
+#' @return Returned is the object of class \code{list} containing the results of TimeTeller cross-validation analysis
+#' @export
+#'
+#' @examples
+#'
+#'
+
 
 train_cv <- function(group_to_leave_out = 'group_1', genes, exp_matrix, test_grouping_vars, group_1, group_2, group_3, time, replicate, mat_normalised = TRUE,
                      treat_independently = TRUE, combine_for_norm = FALSE, parallel_comp = FALSE, cores = 4,
